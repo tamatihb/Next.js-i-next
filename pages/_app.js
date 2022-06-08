568a0a89d32880b926c0b132c52ade9185441301
Tamati <Tamatijames9@gmail.com>
2022-06-08 14:14:43 +1000

    LATEST COMMIT TO FIX VERCEL DEPLOY

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Script from "next/script";
+import { Fragment } from "react";
+
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-119JDZXFHR"></script>
+// <script>
+  // window.dataLayer = window.dataLayer || [];
+  // function gtag(){dataLayer.push(arguments);}
+  // gtag('js', new Date());
+
+  // gtag('config', 'G-119JDZXFHR');
+// </script>
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Script 
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`} 
+      strategy="afterInteractive" />
+      <Script
+      id="google-analytics-script"
+      strategy="afterInteractive"
+       >{`  
+       window.dataLayer = window.dataLayer || [];
+       function gtag(){dataLayer.push(arguments);}
+       gtag('js', new Date());
+
+       gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
+
+       `}
+       </Script>
+      <Component {...pageProps} />;
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
